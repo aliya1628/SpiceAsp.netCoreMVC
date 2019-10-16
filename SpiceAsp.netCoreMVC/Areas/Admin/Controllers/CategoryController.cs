20df718c594c2821304def63962502fc2e1b4e57
Codeest1628 <54244127+Codeest1628@users.noreply.github.com>
2019-10-16 16:39:33 +0530

    Initial commit

---
+++ SpiceAsp.netCoreMVC/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SpiceAsp.netCoreMVC.Data;
+
+namespace SpiceAsp.netCoreMVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //GET
+        public async Task<IActionResult> Index()
+        {
+
+            return View(await _context.Category.ToListAsync());
+        }
+    }
+}
